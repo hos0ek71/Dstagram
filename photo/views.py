609b09508cf4bc866de84cd9337a794f4b27f12f
johoseok <kmb5037@naver.com>
2024-07-17 00:12:29 +0900

    Please enter the commit message for your changes. Lines starting

--- photo/views.py
+++ photo/views.py
@@ -2,41 +2,34 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-
 from .models import Photo
 
+@login_required
 def photo_list(request):
     photos = Photo.objects.all()
     return render(request, 'photo/list.html', {'photos': photos})
 
-class PhotoUploadView(CreateView):
+class PhotoUploadView(LoginRequiredMixin, CreateView):
     model = Photo
     fields = ['photo', 'text']
     template_name = 'photo/upload.html'
+    success_url = '/'
 
     def form_valid(self, form):
         form.instance.author_id = self.request.user.id  # 'instace'를 'instance'로 수정
         if form.is_valid():
             form.instance.save()
-            return redirect('/')
+            return redirect(self.success_url)
         else:
             return self.render_to_response({'form': form})
 
-class PhotoDeleteView(DeleteView):
+class PhotoDeleteView(LoginRequiredMixin, DeleteView):
     model = Photo
     success_url = '/'
     template_name = 'photo/delete.html'
 
-class PhotoUpdateView(UpdateView):
+class PhotoUpdateView(LoginRequiredMixin, UpdateView):
     model = Photo
     fields = ['photo', 'text']
     template_name = 'photo/update.html'
-
-@login_required
-def photo_list(request):
-
-class PhotoUploadView(LoginRequiredMixin, CreateView):
-
-class PhotoDeleteView(LoginRequiredMixin, DeleteView):
-
-class PhotoUpdateView(LoginRequiredMixin, UpdateView):
+    success_url = '/'
